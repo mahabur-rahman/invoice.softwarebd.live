28c23bca231c1a6b9a51d23ebabe78deb19cbaea
ranagithubrr <ranarr.dev@gmail.com>
2025-11-11 13:06:51 +0600

    feat: business page added

--- components/layout/menuItems.tsx
+++ components/layout/menuItems.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { FaHome } from "react-icons/fa";
-import { FiFileText, FiSettings, FiUsers } from "react-icons/fi";
+import { FiBriefcase, FiFileText, FiSettings, FiUsers } from "react-icons/fi";
 import { MdOutlineReceiptLong } from "react-icons/md";
 import type { MenuProps } from "antd";
 
@@ -11,29 +11,34 @@ export const menuItems: MenuProps["items"] = [
     label: <Link href="/">Home</Link>,
   },
   {
-    key: "sub1",
+    key: "2",
     icon: <FiFileText size={18} />,
     label: "Invoice",
     children: [
       {
-        key: "2",
+        key: "101",
         icon: <MdOutlineReceiptLong size={16} />,
         label: <Link href="/invoices">Invoices</Link>,
       },
       {
-        key: "3",
+        key: "102",
         icon: <FiFileText size={16} />,
         label: <Link href="/generate-invoice">Create Invoice</Link>,
       },
       {
-        key: "4",
+        key: "103",
         icon: <FiUsers size={16} />,
         label: <Link href="/clients">Clients</Link>,
+      },
+        {
+        key: "104",
+        icon: <FiBriefcase size={16} />,
+        label: <Link href="/my-business">My Business</Link>,
       },
     ],
   },
   {
-    key: "5",
+    key: "3",
     icon: <FiSettings size={18} />,
     label: <Link href="/settings">Settings</Link>,
   },
